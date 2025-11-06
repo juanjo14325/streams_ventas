6468bac98c1f5d5ad72eee6a77749622652b6ba4
Juan José Ferreri <jferaco483@g.educaand.es>
2025-11-06 14:05:33 +0100

    Aa

--- src/main/java/org/iesvdm/ventas/controller/DemothController.java
+++ src/main/java/org/iesvdm/ventas/controller/DemothController.java
@@ -1,5 +1,6 @@
 package org.iesvdm.ventas.controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.iesvdm.ventas.modelo.Cliente;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,5 +35,28 @@ public class DemothController {
 
         model.addAttribute("escritores",list);
         return "plantilla2";
+    }
+
+    @GetMapping("/demoth3")
+    public String demothHttpSession(Model model, HttpSession session){
+
+        String mensajeASesion = "Lo grabé en demoth 3";
+
+        //atributos en model solo visibles en esta plantilla
+        String mensajeAModel = "Esto sólo lo ve la plantilla demoth3";
+
+        model.addAttribute("msgModel", mensajeAModel);
+
+        //atributos en sesión son visibles en todos los endpoint
+        session.setAttribute("msgSession",mensajeASesion);
+
+        return "demoth3";
+
+    }
+    @GetMapping("/demoth3_2")
+    public String demothHttpSession2(Model model, HttpSession session){
+
+        return "demoth3_2";
+
     }
 }
